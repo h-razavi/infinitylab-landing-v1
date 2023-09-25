1b76dccce6e34308ba08795ddc747ac6ecbcc850
Hossain Razavi <hossain.razavi@gmail.com>
2023-09-25 10:57:43 +0330

    writing main page copy

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -16,7 +16,20 @@ function Carousel() {
               <Image src={community} alt="community" height={160} />
             </div>
             <div className="h-full w-full bg-primary rounded-lg grid place-items-center absolute top-0 opacity-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500">
-              <p className="text-black font-bold">این متن نوشته ماست</p>
+              <p className="text-black text-sm font-bold p-3 text-justify">
+                در <b>اینفینیتی لب</b> ما بر این باوریم که ایده‌ها چه کوچک و چه
+                بزرگ شایسته حمایت هستند. به همین دلیل ما جامعه‌ای از کارآفرینان،
+                سرمایه گذاران، منتورها و متخصصین در رشته‌های مختلف را در کنار هم
+                گرد آورده‌ایم تا به شما کمک کنند آن چه در ذهن دارید را به واقعیت
+                تبدیل کنید. <br />
+                ما شما را با شرکای بالقوه، مشتریان و سرمایه گذارانی که می توانند
+                سرمایه و بازخورد لازم برای رشد کسب و کار شما را فراهم کنند، در
+                ارتباط قرار می دهیم. ما همچنین به شما کمک می کنیم تا به بهترین
+                ابزار و خدماتی که می توانند محصول و عملیات شما را بهبود بخشند،
+                دسترسی پیدا کنید. با استارت آپ استودیو، شما در سفر خود تنها
+                نیستید. شما یک تیم از حامیان دارید که به موفقیت شما پایبند
+                هستند.
+              </p>
             </div>
           </div>
 
@@ -25,7 +38,18 @@ function Carousel() {
               <Image src={speed} alt="speed" height={160} />
             </div>
             <div className="h-full w-full bg-primary rounded-lg grid place-items-center absolute top-0 opacity-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500">
-              <p className="text-black font-bold">این متن نوشته ماست</p>
+              <p className="text-black text-sm font-bold p-3 text-justify">
+                یکی از بزرگترین چالش استارت‌آپها زمان زیاد تبدیل ایده‌ها به
+                محصول و عرضه آن به بازار است. در <b>اینفینیتی لب</b> ما برای حل
+                این چالش مجموعه‌ای از خدمات و برنامه‌ها را به شما ارائه خواهیم
+                نمود. <br />
+                ما به شما کمک می‌کنیم در کمترین زمان ممکن ایده‌‌های خود را
+                ارزیابی کنید، نمونه اولیه را طراحی کرده و با تکرار و اصلاح
+                ویژگی‌های محصول خود در کمترین زمان وارد بازار شوید.تیم متخصصان و
+                منتورهای حاضر در جامعه ما آماده‌اند تا در طول این فرآیند به شما
+                بازخورد، مشاوره و پشتیبانی ارائه کنند تا با صرفه‌جویی در زمان،
+                سرمایه و تلاش محصولی کارآمد و با مقیاس‌پذیری بالا داشته باشید.
+              </p>
             </div>
           </div>
           <div className="w-1/3 p-8 group relative">
@@ -33,7 +57,18 @@ function Carousel() {
               <Image src={social} alt="social" height={160} />
             </div>
             <div className="h-full w-full bg-primary rounded-lg grid place-items-center absolute top-0 opacity-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500">
-              <p className="text-black font-bold">این متن نوشته ماست</p>
+              <p className="text-black text-sm font-bold p-3 text-justify">
+                در <b>اینفینیتی لب</b> هدف ما تنها ساختن استارت‌آپهای موفق نیست. ما
+                می‌خواهیم بر روی فرهنگ و جامعه استارت‌آپی اثر مثبتی بر جا
+                بگذاریم. <br />
+                 ما بر این باوریم که در روزگار و جغرافیای حاضر،
+                استارت‌آپها قدرت حل بسیاری از مشکلات ضروری را دارند. هدف ما
+                حمایت از ایده‌هایی با چشم‌انداز روشن، ماموریتی شفاف و تاثیر
+                اجتماعی و محیطی مثبت است. ما می‌خواهیم با قدرت بخشیدن به انسانها
+                فارق از پیش‌زمینه، تحصیلات، یا موقعیت اجتماعی یک اکوسیستم پویا و
+                متنوع بسازیم. در جامعه اینفینیتی لب شما هم می‌توانید بخشی از
+                جنبشی آینده‌ساز و متفاوت باشید.
+              </p>
             </div>
           </div>
         </div>
